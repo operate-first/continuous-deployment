d1994c550df2efa50e891c8a4adb9a54ae3488bd
Humair Khan <humair88@hotmail.com>
2020-09-24 15:17:40 -0400

    Add dex client secret via env var and use cluster alias in app

--- examples/configure_development.sh
+++ examples/configure_development.sh
@@ -3,11 +3,6 @@
 
 # Retrieve the ArgoCD route, api rest server url, and configure the redirect uri.
 ARGOCD_ROUTE=https://$(oc get route argocd-server -n aicoe-argocd-dev -o jsonpath='{.spec.host}')
-API_SERVER=$(oc whoami --show-server)
-
-# Set up the issuer secret for the dex config:
-OAUTH_TOKEN="$(echo -e "$(oc sa get-token argocd-dex-server -n aicoe-argocd-dev | base64)" | tr -d '[:space:]')"
-oc patch secret argocd-secret -n aicoe-argocd-dev --type='json' -p="[{'op': 'add', 'path': '/data/dex.serviceaccount.clientSecret', 'value':'${OAUTH_TOKEN}'}]"
 
 # Update Issuer and ArgoCD URL in dex config:
 oc -n aicoe-argocd-dev get configmap argocd-cm -o yaml | sed "s#ARGOCD_ROUTE#${ARGOCD_ROUTE}#g" | oc replace -f -
@@ -21,11 +16,9 @@ CLUSTER_SPEC_CONFIG=$(echo -n "{\"bearerToken\":\"${ARGOCD_MANAGER_TOKEN}\",\"tl
 oc patch secret dev-cluster-spec -n aicoe-argocd-dev --type='json' -p="[{'op': 'add', 'path': '/data/config', 'value':'${CLUSTER_SPEC_CONFIG}'}]"
 
 # We also need to provide the cluster spec with the location of this server.
+API_SERVER=$(oc whoami --show-server)
 API_SERVER_ENCODED=$(echo -ne ${API_SERVER} | base64 | tr -d '[:space:]')
 oc patch secret dev-cluster-spec -n aicoe-argocd-dev --type='json' -p="[{'op': 'add', 'path': '/data/server', 'value':'${API_SERVER_ENCODED}'}]"
-
-# Update the example dev application to point to the dev cluster
-oc -n aicoe-argocd-dev get applications dev-app -o yaml | sed "s#API_SERVER#${API_SERVER}#g" | oc replace -f -
 
 # The default application that's created belongs to the dev-team project which
 # is only accessible by the dev group. So we add you to the group here:
